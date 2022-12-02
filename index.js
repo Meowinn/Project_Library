23e8239afd1b55b50dccd3d21f26d3756ff625a8
equinox <darwinfagarang@gmail.com>
2022-12-02 14:34:13 +0800

    correct bug resulting to disorganization. add additional styles

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const addBook = document.getElementById('addBook');
 const form = document.querySelector('form');
 const submit = document.getElementById('submit');
 const input_fields = document.querySelectorAll('input');
+
+const form_read = document.querySelector('#read');
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -11,18 +14,10 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    /* this.info = function() {
-        return console.log(title,author,pages,read);
-    }*/
-}
-/*
-Book.prototype.chapters = function() {
-    console.log("16");
-}
-*/
+    };
 
 function addBookToLibrary(item) {
-    myLibrary.push(new Book(title.value, author.value, pages.value));
+    myLibrary.push(new Book(title.value, author.value, pages.value, form_read.checked));
 
     const parentContainer = document.createElement('div');
     parentContainer.setAttribute('id', 'parentContainer')
@@ -33,27 +28,13 @@ function addBookToLibrary(item) {
     closeButton.appendChild(closeText);
     parentContainer.appendChild(closeButton);
 
-    let internalCheck = document.createElement('input');
-    internalCheck.type = "checkbox";
-    internalCheck.name = "read";
-
-    let internalChecktext = document.createTextNode("Read");
-    parentContainer.appendChild(internalChecktext);
-    parentContainer.appendChild(internalCheck);
-    
-    internalCheck.addEventListener('click', function(){
-        if (internalCheck.checked == true) {
-            console.log("I am checked");
-            myLibrary[0].read = "true";
-        } else {myLibrary[0].read = "false"};
-    });
-
     closeButton.addEventListener('click', function(){
         myLibrary.splice(myLibrary.indexOf(item),1);
         parentContainer.remove();
     });
 
     for (let data in myLibrary[myLibrary.length-1]){
+
         console.log(myLibrary[myLibrary.length-1]);
         const parentBook = document.createElement(`div`);
         parentBook.setAttribute('id', 'parentBook')
@@ -61,6 +42,52 @@ function addBookToLibrary(item) {
         card_container.appendChild(parentContainer);
         parentContainer.appendChild(parentBook);
     };
+
+    let status = document.querySelectorAll('#parentBook:nth-child(5)');
+    for (const readStatus of status){
+        readStatus.style.display = 'none';
+    }
+
+    let readButtons = document.createElement('button');
+    let readText = document.createTextNode('READ');
+    readButtons.classList.add('readButtons');
+    let readButtons2 = document.createElement('button');
+    let readText2 = document.createTextNode('NOT READ');
+    readButtons2.classList.add('readButtons2')
+
+    if(form_read.checked == true) {
+        readButtons.appendChild(readText);
+        parentContainer.appendChild(readButtons); 
+         }else { 
+        readButtons2.appendChild(readText2);
+        parentContainer.appendChild(readButtons2); 
+         }
+    
+    readButtons.addEventListener('click', function(){
+            if(readText.nodeValue == "READ"){
+                readText.nodeValue = "NOT READ";
+                myLibrary[0].read = false;
+                readButtons.classList.add('readButtons2');
+            }else{
+                readText.nodeValue = "READ"
+                myLibrary[0].read = true;
+                readButtons.classList.remove('readButtons2');
+             };
+        })
+
+    readButtons2.addEventListener('click', function(){
+            if(readText2.nodeValue == "NOT READ"){
+                readText2.nodeValue = "READ";
+                myLibrary[0].read = false;
+                readButtons2.classList.remove('readButtons2');
+                readButtons2.classList.add('readButtons');
+            }else{
+                readText2.nodeValue = "NOT READ"
+                myLibrary[0].read = true;
+                readButtons2.classList.remove('readButtons');
+                readButtons2.classList.add('readButtons2');
+             };
+        })
 }
 
 addBook.addEventListener(`click`, ()=> {
@@ -75,7 +102,6 @@ submit.addEventListener('click', ()=> {
     event.preventDefault();
     addBookToLibrary();
     form.setAttribute('id', 'form_visible');
-    
 });
 
 document.getElementById('close').addEventListener(`click`, () => {
